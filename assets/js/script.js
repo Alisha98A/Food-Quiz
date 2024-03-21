6458c20a01cc7d6fc4fb74e4a071bf426097fef8
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-21 17:06:26 +0100

    Hide oval circle onClick Play button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
-
+document.getElementById("playBtn").addEventListener("click", function () {
+  // Hide the oval circle
+  document.getElementById("ovalCircleContainer").style.display = "none";
+});
