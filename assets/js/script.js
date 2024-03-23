6242da2e4e308ce40d97190b946c0385a256ccf6
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-23 11:19:34 +0100

    Add comment in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+// GAME PAGE
 document.getElementById("playBtn").addEventListener("click", function () {
-  // Hide the oval circle
+  // Hide the oval circle when Play button is clicked on
   document.getElementById("ovalCircleContainer").style.display = "none";
+
 });
