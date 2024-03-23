0083c88396e8b51abc0d5c9af280628e58bd635b
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-23 17:48:02 +0100

    Implement JavaScript navigation for Play button

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+// Add event listener to the "Play" button
+document.getElementById("playBtn").addEventListener("click", function () {
+  // Click on 'Play' button to get redirected to the quiz (game page)
+  window.location.href = "game.html";
+});
