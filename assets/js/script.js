570b26a78cfcc3f78f0ffa72b377710abfa7c646
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-23 11:52:34 +0100

    Edit get ready text in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,4 @@
 document.getElementById("playBtn").addEventListener("click", function () {
   // Hide the oval circle when Play button is clicked on
   document.getElementById("ovalCircleContainer").style.display = "none";
-
 });
