06ce3d5631c07ac49d1c2106c3ee0bbe716f3725
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-24 14:32:21 +0100

    Add DOM elements and game variables to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // DOM elements
+  const questionElement = document.getElementById("question");
+  const options = Array.from(document.querySelectorAll(".answer-text"));
+  const nextButton = document.getElementById("nextBtn");
+  const scoreValue = document.getElementById("score-value");
+
+  // Game variables
+  let currentQuestion = {};
+  let score = 0;
+  let questionCounter = 0;
+  let availableQuestions = [];
+
+  
+});
