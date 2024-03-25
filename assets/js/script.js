68924dfa6652262568349d428354873d2612e05e
Alisha Andersson <alishaanderssonn@icloud.com>
2024-03-25 20:38:04 +0100

    Listen for click on 'Game Rules' btn to display modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-
 // Add event listener to the "Play" button
 document.getElementById("playBtn").addEventListener("click", function () {
   // Click on 'Play' button to get redirected to the quiz (game page)
   // (Learned how redirect to another webpage from https://www.geeksforgeeks.org/how-to-redirect-to-another-webpage-using-javascript/)
   window.location.href = "game.html";
+});
+// Add event listener to the "Game Rules" button
+document.getElementById("rulesBtn").addEventListener("click", function () {
+  // Display the modal when the button is clicked
+  document.getElementById("rulesModal").style.display = "block";
 });
